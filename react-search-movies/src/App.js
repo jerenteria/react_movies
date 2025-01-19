d44b12bef90f2c9cc48924906a462be90b47a80c
jerenteria <renteria.juan96@gmail.com>
2025-01-19 14:42:35 -0800

    not able to get side bar working

--- react-search-movies/src/App.js
+++ react-search-movies/src/App.js
@@ -1,44 +1,55 @@
-import './App.css';
-import React, { useState } from 'react';
-import MovieList from './components/MovieList';
-import SideNav from './components/SideNav';
+import "./App.css";
+import React, { useState } from "react";
+import MovieList from "./components/MovieList";
+import SideNav from "./components/SideNav";
 
 function App() {
   const [movies, setMovies] = useState([]);
-  const [query, setQuery] = useState('');
+  const [query, setQuery] = useState("");
 
   const options = {
-    method: 'GET',
+    method: "GET",
     headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYjliYzM1OTE5M2EzM2VhNmFiZTdlNTg0NTE5Njk3NSIsInN1YiI6IjY1MTA5YjdiZTFmYWVkMDBhZTMwNTFmMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Z6JMTBzW0QPdEaVD3rn196Txl0WaT0LeY2gbxct7ffc'
-    }
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYjliYzM1OTE5M2EzM2VhNmFiZTdlNTg0NTE5Njk3NSIsInN1YiI6IjY1MTA5YjdiZTFmYWVkMDBhZTMwNTFmMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Z6JMTBzW0QPdEaVD3rn196Txl0WaT0LeY2gbxct7ffc",
+    },
   };
 
-  fetch('https://api.themoviedb.org/3/discover/movie?api_key=3b9bc359193a33ea6abe7e5845196975', options)
-    .then(response => response.json())
-    .then(data => setMovies(data.results)) // call the response data and setMovies to data and reults is the name of arr in json response
-    .catch(err => console.error(err));
+  fetch(
+    "https://api.themoviedb.org/3/discover/movie?api_key=3b9bc359193a33ea6abe7e5845196975",
+    options
+  )
+    .then((response) => response.json())
+    .then((data) => setMovies(data.results)) // call the response data and setMovies to data and reults is the name of arr in json response
+    .catch((err) => console.error(err));
 
   // if we havent searched for anything yet show all the movies
-  const filterMovies = query.length === 0 ? movies :
-    // if not filter the results by the input(get event.target.value onChange!)
-    movies.filter(movies => movies.original_title.toLowerCase().includes(query.toLowerCase()));
-
+  const filterMovies =
+    query.length === 0
+      ? movies
+      : // if not filter the results by the input(get event.target.value onChange!)
+        movies.filter((movies) =>
+          movies.original_title.toLowerCase().includes(query.toLowerCase())
+        );
 
   return (
     <div className="App">
-      <SideNav />
-      <div className="header">
-        <h1 className="header-text">Check Out These Trending Movies!</h1>
-        <input className="search"
-          type="search"
-          placeholder="Search Movie..."
-          onChange={e => {
-            setQuery(e.target.value)
-          }} />
+      <div className="app-container">
+        <SideNav />
+        <div className="header">
+          <h1 className="header-text">Check Out These Trending Movies!</h1>
+          <input
+            className="search"
+            type="search"
+            placeholder="Search Movie..."
+            onChange={(e) => {
+              setQuery(e.target.value);
+            }}
+          />
+        </div>
+        <MovieList movies={filterMovies} />
       </div>
-      <MovieList movies={filterMovies} />
     </div>
   );
 }
